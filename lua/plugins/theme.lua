016815014005e3d1847a7acccc8aa3f2700606c9
Caner Derici <caner@dericilab.live>
2026-04-16 14:34:20 -0600

    update(theme): lighter bg

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,21 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "tokyonight-night",
-		},
-	},
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    opts = {
+      style = "night",
+      on_highlights = function(hl, c)
+        hl.Comment = {
+          fg = "#7a88cf",
+          italic = true,
+        }
+      end,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
 }
